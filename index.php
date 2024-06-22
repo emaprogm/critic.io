2720fd6d5ae024a20f6b07654e3c0db5327b74c3
michelamaniprog <michelamaniprogwm@gmail.com>
2024-06-22 23:18:02 +0000

    refresh

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="critic.css">
+    <link rel="shortcut icon" href="images/Logo/logo.jpg">
+    <link rel="stylesheet" href="fontawesome-free-6.1.1-web/css/fontawesome.css">
+    <link rel="stylesheet" href="fontawesome-free-6.1.1-web/css/fontawesome.min.css">
+    <link rel="stylesheet" href="fontawesome-free-6.1.1-web/css/all.css">
+    <title>CriTic</title>
+</head>
+<body>
+    <?php 
+        include("header.php");
+        include("toggle.php");
+        include("illustrationPagePrincipale.php");
+        include("sectionArticles.php");
+        include("SectionServices.php");
+        include("sectionTeam.php");
+        include("sectionLaisserSMS.php");
+        include("sectionPartenaires.php");
+        include("footer.php");
+
+    ?>
+</body>
+</html>
